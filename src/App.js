06be04a0ad5c840b8f83ea8bbc5e489e886235bc
Felipe <fquiceno1@misena.edu.co>
2022-02-23 18:20:56 -0500

    se termina video 4 para validacion de formulario, con la novedad de que no se pudo manejar el error correctamente ya que la libreria react-hook-form cambio, queda pendiente consultar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 //import Contador from './components/Contador';
 //import Jsx from './components/Jsx';
 //import Lista from './components/Lista';
-import Formulario from './components/Formulario';
-
+//import Formulario from './components/Formulario';
+import FormHook from './components/FormHook';
 
 function App() {
   return (
     //ClassName para ingresar elementos Boostrap css, mt-5 : magin top, 
     <div className="container mt-5">         
-      <Formulario/>                           
+      <FormHook/>                           
     </div>
   );
 }
---
+++ src/components/FormHook.js
@@ -0,0 +1,40 @@
+import React, {Fragment} from 'react';
+
+import { useForm } from 'react-hook-form'
+/**
+ * La gracia de trabajar con react-Formhook , es que 
+ * podemos validar el formulario de una forma muy sencilla
+ * 1) Lo primero es importarlo : import {useForm} from 'react-hook.form'
+ * 2) onSubmit={handleSubmit(onSubmit)} : con esta linea estamos evaluando cada uno de los inputs
+ *  Nota: para poder evaluarlños, deben de tener el name
+ * 3) El register , nos permite evaluar el dato
+ */
+
+const FormHook = () => {
+
+    const {register, errors, handleSubmit} = useForm();
+
+    //cada vez que presionemos el boton leemos todos los inputs
+    const onSubmit =(data,e) => {
+        console.log(data)
+        e.target.reset()
+    }
+
+    return ( 
+        <Fragment>
+            <h1>Formulario</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    name = "titulo"
+                    className='form-control my-2'                                       //margin arriba y abajo de 2
+                    
+                    {...register("titulo", { required: true })}
+                />
+           {/* {errors.exampleRequired && <span>This field is required</span>}   Este campo no quiere funcionar, Consultarlo*/  }
+                <button className='btn btn-primary'>Enviar</button>
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default FormHook;
