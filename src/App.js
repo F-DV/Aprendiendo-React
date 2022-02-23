9baf0356e047c4c1b57232f07e30db28e60e0651
Felipe <fquiceno1@misena.edu.co>
2022-02-23 15:48:21 -0500

    Video 3, En el cual aprendimos a crear formularios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Contador from './components/Contador';
-import Jsx from './components/Jsx';
-import Lista from './components/Lista';
+//import Contador from './components/Contador';
+//import Jsx from './components/Jsx';
+//import Lista from './components/Lista';
 import Formulario from './components/Formulario';
 
 
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo React</h1>
-      <Jsx />
-      <Lista />
-
+    //ClassName para ingresar elementos Boostrap css, mt-5 : magin top, 
+    <div className="container mt-5">         
+      <Formulario/>                           
     </div>
   );
 }
