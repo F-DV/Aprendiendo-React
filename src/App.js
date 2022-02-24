201f93f9abeb6447f273cdaf3ffa1ddf6d953f16
Felipe <fquiceno1@misena.edu.co>
2022-02-24 08:55:30 -0500

    Se termina video #1 sobre Router,se tuvieron problemas con la libreria

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from 'react';
-//import Contador from './components/Contador';
-//import Jsx from './components/Jsx';
-//import Lista from './components/Lista';
-//import Formulario from './components/Formulario';
-import FormHook from './components/FormHook';
-import EjemploUno from './components/EjemploUno';
-import Saludo from './components/Saludo';
-import Comentario from './components/Comentario';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Contacto from './components/Contacto';
+import Inicio from './components/Inicio'; 
+import Nosotros from './components/Nosotros';
 
 function App() {
 
-  const sujeto = {
-    nombre: 'Juanito',
-    UrlImagen: 'https://via.placeholder.com/64',
-    texto: 'Esto lo escribio juanito un dia que tenia ganas de escribir'
-  }
-  return (
-    //ClassName para ingresar elementos Boostrap css, mt-5 : magin top, 
-    <div className="container mt-5">         
-      {/*<FormHook/>*/}
-      {/*<EjemploUno/>*/}
-      <h2>Components</h2>
-      <Saludo persona="Juanito"/>
-      <Saludo persona="Felipe"/> 
-      <Saludo persona="Catalina"/>
-      <Comentario sujeto={sujeto} /> 
-      <Comentario sujeto={sujeto} /> 
 
-    </div>
+  return (
+
+    //ClassName para ingresar elementos Boostrap css, mt-5 : magin top, 
+    <Router>
+      <div className="container mt-5">
+        <div className='btn-group'>
+          <Link to="/" className='btn btn-dark'>
+          Inicio
+          </Link>
+          <Link to="/nosotros" className='btn btn-dark'>
+          Nosotros
+          </Link>
+          <NavLink to="/contacto" className='btn btn-dark' ctiveClassName='active' >
+          Contacto
+          </NavLink>
+          </div> 
+        <h1>Navbar...</h1>
+        <hr/>
+        <Switch>
+          <Route path = "/" exact>
+            <Inicio/>
+          </Route>
+          <Route path = "/contacto">
+            <Contacto />
+          </Route>
+          <Route path = "/nosotros">
+          <Nosotros />
+          </Route>
+        </Switch>
+       
+      </div>
+    </Router>
+
+    
   );
 }
 
